babd9a02252ecc9c6479a9b4cc0fbccadc4f7148
Irek Romaniuk <irekromaniuk@ireks-air.lan>
2019-03-24 08:07:59 -0400

    blog updated

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 from blog.models import Post
+from . import CommentForm
  
 def blog_index(request):
     posts = Post.objects.all().order_by('-created_on')
@@ -22,12 +23,25 @@ def blog_category(request, category):
         "posts": posts
     }
     return render(request, "blog_category.html", context)   
+
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+ 
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
+            )
+            comment.save()
+
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form": CommentForm(),
     }
- 
-    return render(request, "blog_detail.html", context) 
+    return render(request, "blog_detail.html", context)    
