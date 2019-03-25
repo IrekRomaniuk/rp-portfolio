828de9db5568044a4b2909fc5e89f144db9320b9
Irek Romaniuk <irekromaniuk@ireks-air.lan>
2019-03-25 06:55:42 -0400

    updating blog

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 from blog.models import Post
-from . import CommentForm
+from blog.forms import CommentForm
  
 def blog_index(request):
     posts = Post.objects.all().order_by('-created_on')
